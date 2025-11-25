46fb5c695e4325ea81db7fbce9e86a05032aa352
kevin012106mg12-art <kevin012106.mg12@nycu.edu.tw>
2025-11-25 14:30:45 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 long long factorial(int n) {
-    if (n == 1 || n == 0)
+    if (n == 1)
         return 1;
     return n * factorial(n - 1);
 }
 
 long long sum(int n) {
-    if (n == 0)
-        return 0;
+    if (n == 1)
+        return 1;
     return n + sum(n - 1);
 }
 
